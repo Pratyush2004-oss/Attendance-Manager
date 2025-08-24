c7b3bef6d0b953f0a0877c9fb474922b55cf1a37
Pratyush2004-oss <pratyush@sitp.ac.in>
2025-08-24 18:14:28 +0530

    Initializing frontend

---
+++ Attendance-Manager/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="items-center justify-center flex-1 bg-red-500">
+      <Text className="text-2xl text-white">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
